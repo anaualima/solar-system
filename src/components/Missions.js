5e1adc736c6c81b12424f43c9e23a67245959e75
Alissandra <alissandraraysa@gmail.com>
2021-10-29 18:20:50 -0300

    requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h1>oi</h1>
+      </div>
+    );
+  }
+}
